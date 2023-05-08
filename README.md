ef07600839094c50d66aae6f9c54d6488cb6eb60
Adam Stirtan <adamstirtan@gmail.com>
2023-05-07 21:52:16 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# discord_gpt_bot
-A Discord bot in Python that responds to @ messages with the OpenAI API
+# Overview
+
+A Discord bot in Python that responds to @ messages with the OpenAI API for text completion.
+
+## 😎 Create virtual environment
+
+```python
+python3 -m venv my_env
+```
+
+Now set the virtual environment as the current.
+
+```python
+source my_env/bin/activate
+```
+
+## 💾 Install libraries
+
+```python
+pip install discord
+pip install openai
+pip install python-dotenv
+```
+
+## 🖥️ Set environment variables
+
+Create a `.env` file in application root and set two values for the bot's API credentials:
+
+```python
+DISCORD_TOKEN = your-token
+OPENAI_KEY = your-key
+```
+
+## 🚀 Start the bot
+
+```python
+python3 bot.py
+```
---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+import discord
+import openai
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+opneai_key = os.getenv('OPENAI_KEY')
+
+openai.api_key = opneai_key
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix = '!', intents = intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    prompt = message.content
+        
+    if client.user.mentioned_in(message):
+        response = openai.Completion.create(
+            engine = 'text-davinci-003',
+            prompt = prompt,
+            temperature = 0.5,
+            max_tokens = 256,
+            top_p = 1.0,
+            frequency_penalty = 0.0,
+            presence_penalty = 0.0
+        )
+
+    await message.channel.send(response.choices[0].text)
+
+client.run(token)
